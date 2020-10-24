cdbfb5dd98e02b0781fa17fa9798d5168b31180a
EGAMAGZ <gazapito1@hotmail.com>
2020-10-24 10:11:13 -0500

    Basic div that can slide

--- js/homepage.js
+++ js/homepage.js
@@ -1,5 +1,26 @@
+(function(){
+
+
+})();
+
 const scroll_arrow_element = document.getElementById('scroll-arrow');
+const page_welcome = document.getElementById('page-welcome');
+const page_container = document.getElementById('page-container');
+const show_button = document.getElementById('show')
+
+const hide_page_container = ()=>{
+    if(page_container.classList.contains('hide')){
+        page_container.classList.remove('hide');
+        page_container.classList.add('show');
+    }
+    else{
+        page_container.classList.remove('show')
+        page_container.classList.add('hide');
+    }
+}
 
 scroll_arrow_element.addEventListener('click', ()=>{
-    alert("Click");
+    hide_page_container();
 });
+
+show_button.addEventListener('click', () => hide_page_container())
