6c15e1827262c06c781e78a206298324b7eafc28
EGAMAGZ <gazapito1@hotmail.com>
2020-10-24 11:28:37 -0500

    Added navbar with its own styles

--- js/homepage.js
+++ js/homepage.js
@@ -1,12 +1,7 @@
-(function(){
-
-
-})();
-
 const scroll_arrow_element = document.getElementById('scroll-arrow');
 const page_welcome = document.getElementById('page-welcome');
 const page_container = document.getElementById('page-container');
-const show_button = document.getElementById('show')
+const show_button = document.getElementById('showWelcome')
 
 const hide_page_container = ()=>{
     if(page_container.classList.contains('hide')){
