286f98f74290d18e2f769966919e2f5846a9af83
EGAMAGZ <gazapito1@hotmail.com>
2020-11-16 20:13:02 -0600

    Added sample sidebar

--- js/homepage.js
+++ js/homepage.js
@@ -1,21 +1,40 @@
+const menu_button = document.getElementById('menuToogle');
+const close_nav_button = document.getElementById('closeNav');
 const scroll_arrow_element = document.getElementById('scroll-arrow');
 const page_welcome = document.getElementById('page-welcome');
 const page_container = document.getElementById('page-container');
-const show_button = document.getElementById('showWelcome')
+const show_button = document.getElementById('showWelcome');
+const side_bar = document.getElementById("sideBar");
 
-const hide_page_container = ()=>{
-    if(page_container.classList.contains('hide')){
+const display_page_container = () => {
+    if (page_container.classList.contains('hide')) {
         page_container.classList.remove('hide');
         page_container.classList.add('show');
     }
-    else{
+    else {
         page_container.classList.remove('show')
         page_container.classList.add('hide');
     }
 }
 
-scroll_arrow_element.addEventListener('click', ()=>{
-    hide_page_container();
-});
+const show_sidenav = () => {
+    if (side_bar.classList.contains('sidenav-hide')) {
+        side_bar.classList.remove('sidenav-hide');
+        side_bar.classList.add('sidenav-show');
+    }
+}
 
-show_button.addEventListener('click', () => hide_page_container())
+const hide_sidenav = () => {
+    if (side_bar.classList.contains('sidenav-show')) {
+        side_bar.classList.remove('sidenav-show');
+        side_bar.classList.add('sidenav-hide');
+    }
+}
+
+scroll_arrow_element.addEventListener('click', () => display_page_container());
+
+show_button.addEventListener('click', () => display_page_container());
+
+close_nav_button.addEventListener('click', () => hide_sidenav());
+
+menu_button.addEventListener('click', () => show_sidenav());
